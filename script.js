daa7224434adc10e618fbff7855814288cc0e879
juanhdzx <jhernandez12902@gmail.com>
2025-01-06 22:12:29 -0500

    edited nav + scroll feature

--- script.js
+++ script.js
@@ -86,3 +86,25 @@ function toggleAccordion() {
 
 items.forEach(item => item.addEventListener('click', toggleAccordion));
 
+
+//1/6 
+//Script for Scroll to Top feature on bottom rightside
+
+    let mybutton = document.getElementById("myBtn");
+
+    // When the user scrolls down 20px from the top of the document, show the button
+        window.onscroll = function() {scrollFunction()};
+
+    function scrollFunction() {
+    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+        mybutton.style.display = "block";
+    } else {
+        mybutton.style.display = "none";
+    }
+    }
+
+    // When the user clicks on the button, scroll to the top of the document
+    function topFunction() {
+        document.body.scrollTop = 0;
+        document.documentElement.scrollTop = 0;
+    }
